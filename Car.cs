ea8a6c4b4d1307296e2375ba2cd47d1a395e0159
yassin0309 <157421435+yassin0309@users.noreply.github.com>
2025-07-27 18:30:22 +0300

    Taxi App Sys (training)

---
+++ Car.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainingProject
+{
+    internal class Car
+    {
+        public string OwnerName { get; set; }
+        public string CarName { get; set; }
+        public string CarNumber { get; set; }
+        public string CarColor { get; set; }
+        private int CarModel { get; set; }
+    }
+    
+}
